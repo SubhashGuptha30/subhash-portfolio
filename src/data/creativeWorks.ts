2a980232f771cd97056c9ba7a6e39757f41b71d3
Subhash Guptha <subbuguptha2005@outlook.com>
2025-06-15 21:47:37 +0530

    Refactor Hero component for improved readability and maintainability; update creativeWorks and photoGallery data structure

--- src/data/creativeWorks.ts
+++ src/data/creativeWorks.ts
@@ -1,24 +1,26 @@
-
 export const creativeWorks = [
   {
     title: "Vijayanath - A Warrior's Tale",
-    description: "An epic tale of courage and honor set in ancient times, exploring themes of duty and sacrifice.",
+    description:
+      "An epic tale of courage and honor set in ancient times, exploring themes of duty and sacrifice.",
     type: "Script",
-    image: "/uploads/creative-work-1.png",
+    image: "/public/uploads/creative-work-1.png",
     link: "https://docs.google.com/document/d/1OOodgSuOLy_qk2aNxi7Zj4rZyP6UUpA-E4F91JYyqyM/edit?usp=sharing",
   },
   {
     title: "Reflections in Code",
-    description: "A collection of poems inspired by the intersection of technology and human emotion.",
+    description:
+      "A collection of poems inspired by the intersection of technology and human emotion.",
     type: "Poetry",
     image: "https://i.postimg.cc/3x5f1j6H/image.png",
-    link: null
+    link: null,
   },
   {
     title: "Digital Dreams",
-    description: "A short story exploring the relationship between artificial intelligence and human consciousness.",
+    description:
+      "A short story exploring the relationship between artificial intelligence and human consciousness.",
     type: "Story",
     image: "https://i.postimg.cc/3x5f1j6H/image.png",
-    link: null
-  }
+    link: null,
+  },
 ];
