b57aba0db9791e2891a3772acbc3e45f5a7fc97e
Subhash Guptha <subbuguptha2005@outlook.com>
2025-06-17 14:47:54 +0530

    Add additional photos to the gallery for enhanced visual content

--- src/data/personal-space-data.ts
+++ src/data/personal-space-data.ts
@@ -32,8 +32,44 @@ export const photoGallery = [
     alt: "Temple in Annavaram",
   },
   {
+    src: "https://i.postimg.cc/4xLDQV6Q/building-in-cloudy-sky.jpg",
+    alt: "Building in a cloudy sky",
+  },
+  {
+    src: "https://i.postimg.cc/XqgCRVx4/Cradles-of-nature.jpg",
+    alt: "Cradles of nature",
+  },
+  {
+    src: "https://i.postimg.cc/JzXPFXZL/Deer.jpg",
+    alt: "Deer in the forest",
+  },
+  {
+    src: "https://i.postimg.cc/zB2QNrBZ/Deer2.jpg",
+    alt: "Deer in the forest",
+  },
+  {
+    src: "https://i.postimg.cc/XqGnSxnL/Green-Hills.jpg",
+    alt: "Green hills",
+  },
+  {
+    src: "https://i.postimg.cc/T2FDy45R/hands-of-nature.jpg",
+    alt: "Hands of nature",
+  },
+  {
+    src: "https://i.postimg.cc/j5rxfBY7/Moonlight.jpg",
+    alt: "Moonlight",
+  },
+  {
+    src: "https://i.postimg.cc/5ySS1cbq/Night-Walk.jpg",
+    alt: "Night walk in Tiruvanamalai",
+  },
+  {
     src: "https://i.postimg.cc/Y9KcSdkM/Beach-Time.jpg",
     alt: "Beach time",
+  },
+  {
+    src: "https://i.postimg.cc/nz0LtTN9/Tirumala-Hills.jpg",
+    alt: "Tirumala hills",
   },
 ];
 
