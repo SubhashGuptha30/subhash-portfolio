dc723c0de2626dd5d5cf42ba62aa554e7a1efff1
Subhash Guptha <subbuguptha2005@outlook.com>
2025-07-08 23:30:25 +0530

    Update version to 0.0.10 in package.json, adjust favicon path in index.html, and change base path in App.tsx for improved routing.

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/subhash-portfolio">
+        <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/personal-space" element={<PersonalSpace />} />
