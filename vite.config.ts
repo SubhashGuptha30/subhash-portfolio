5c50da74eb599b85b10cc60037b12cd75a7f4844
SubhashGuptha30 <subbuguptha2005@outlook.com>
2025-07-30 18:53:13 +0000

    Add CNAME file for custom domain configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "domain" ? "/" : "/subhash-portfolio/",
+  base: '/',
   server: {
     host: "::",
     port: 8080,
